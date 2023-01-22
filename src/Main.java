390aeaa87692a1df22a71d77886235210239829f
Omar Zorro <danteodzp@gmail.com>
2023-01-22 12:48:32 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int resultado = suma(5,1,4);
+        System.out.println("resultado = " + resultado);
+    }
+
+    public static int suma(int a, int b, int c){
+        return (a + b + c);
+    }
+}
