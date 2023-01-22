27cf553f2ef332891bb22697fb0027f689282f60
Omar Zorro <danteodzp@gmail.com>
2023-01-22 12:55:02 -0500

    Clase coche

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+
+        coche coche1 = new coche(0);
+        coche1.AñadirPuerta();
+        coche1.AñadirPuerta();
+
+        System.out.println("NumeroPuertas = " + coche1.numeroPuertas);
+
         int resultado = suma(5,1,4);
         System.out.println("resultado = " + resultado);
     }
---
+++ src/coche.java
@@ -0,0 +1,11 @@
+public class coche {
+
+    int numeroPuertas;
+    public coche(int numeroPuertas){
+        numeroPuertas = 0;
+    }
+    public void AñadirPuerta(){
+        numeroPuertas++;
+    }
+
+}
